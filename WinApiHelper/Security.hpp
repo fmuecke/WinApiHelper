5335d940fa2c49e322ebfaaa6da81a4c869c9a35
Florian Mücke <fmuecke@baramundi.de>
2015-09-10 05:50:51 +0200

    improved errorhandling

--- WinApiHelper/Security.hpp
+++ WinApiHelper/Security.hpp
@@ -20,14 +20,27 @@ namespace WinApiHelper
 			SID_NAME_USE sidNameUse;
 			DWORD nameBufferSize = 0;
 			DWORD domainBufferSize = 0;
-			::LookupAccountSidW(NULL, pSid.get(), NULL, &nameBufferSize, NULL, &domainBufferSize, &sidNameUse);
-			std::vector<wchar_t> nameBuffer(nameBufferSize, 0);
-			std::vector<wchar_t> domainBuffer(domainBufferSize, 0);
-			ret = ::LookupAccountSidW(NULL,	pSid.get(), nameBuffer.data(), &nameBufferSize, domainBuffer.data(), &domainBufferSize, &sidNameUse) != 0;	
-			if (!ret) return ::GetLastError();
+			ret = ::LookupAccountSidW(NULL, pSid.get(), NULL, &nameBufferSize, NULL, &domainBufferSize, &sidNameUse) != 0;
+            if (!ret)
+            {
+                auto errorCode = ::GetLastError();
+                if (errorCode != ERROR_INSUFFICIENT_BUFFER)
+                {
+                    if (errorCode != ERROR_NONE_MAPPED) return errorCode;
+                    user.assign(sidStr);
+                }
+                else
+                {
+                    std::vector<wchar_t> nameBuffer(nameBufferSize, 0);
+                    std::vector<wchar_t> domainBuffer(domainBufferSize, 0);
+                    ret = ::LookupAccountSidW(NULL, pSid.get(), nameBuffer.data(), &nameBufferSize, domainBuffer.data(), &domainBufferSize, &sidNameUse) != 0;
+                    if (!ret) return ::GetLastError();
 
-			user.assign(nameBuffer.data());
-			domain.assign(domainBuffer.data());
+                    user.assign(nameBuffer.data());
+                    domain.assign(domainBuffer.data());
+                }
+            }
+
 			return NO_ERROR;
 		}		
 	}
