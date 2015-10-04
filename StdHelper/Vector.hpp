df8d8b2220f9c1cf31804aa79eb4f2e22ebf999a
Florian Mücke <fmuecke@gmail.com>
2015-10-04 13:36:25 +0200

    added vector with range checking

---
+++ StdHelper/Vector.hpp
@@ -0,0 +1,22 @@
+#include <vector>
+
+namespace StdHelper
+{
+	// Vector with range checking.
+	// copied from Stroustrup: "A Tour of C++"
+	template<typename T>
+	class Vector : public std::vector<T>
+	{
+	public:
+		using vector<T>::vector; // use the constructors from vector (under the name Vec)
+		T& operator[](int i) // range check
+		{ 
+			return vector<T>::at(i); 
+		}
+		
+		const T& operator[](int i) const // range check const objects; §4.2.1
+		{ 
+			return vector<T>::at(i); 
+		}
+	};
+}
